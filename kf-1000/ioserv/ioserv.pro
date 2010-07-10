c6aafadd0cadd0da8898da0d690b12eed34f5f4e
(no author) <(no author)@76bea514-bf2f-4ad3-ac3e-82dc02406d2c>
2010-07-10 15:04:08 +0000

    Реалізовано карти змінних та програму ioserver
    git-svn-id: file:///tmp/qt4/Linovitsa@268 76bea514-bf2f-4ad3-ac3e-82dc02406d2c

--- kf-1000/ioserv/ioserv.pro
+++ kf-1000/ioserv/ioserv.pro
@@ -1,25 +1,13 @@
-# -------------------------------------------------
-# Project created by QtCreator 2010-02-25T14:01:57
-# -------------------------------------------------
+QT -= gui
 QT += network \
     sql
-QT -= gui
-TARGET = ioserv
-CONFIG -= app_bundle
-CONFIG += debug_and_release
-TEMPLATE = app
-
+OTHER_FILES += list.txt \
+    list2.txt
 HEADERS += reportwr.h
-
-SOURCES += main.cpp \
-    reportwr.cpp
-MOC_DIR = build
-OBJECTS_DIR = build
-UI_DIR = build
-RCC_DIR = build
-OTHER_FILES += list.txt
+SOURCES += reportwr.cpp \
+    main.cpp
 RESOURCES += test.qrc
 QMAKE_LIBDIR += ../../../lib
 LIBS += -lrcada
 INCLUDEPATH += ../../../lib/include
-
+PRE_TARGETDEPS += ../../../lib/librcada.a
--- kf-1000/ioserv/main.cpp
+++ kf-1000/ioserv/main.cpp
@@ -12,7 +12,6 @@
 
 
 //#include "display.h"
-#include "form.h"
 
 int main(int argc, char *argv[])
 {
@@ -28,10 +27,10 @@ int main(int argc, char *argv[])
     QVector<RxModbus*> r;
     QVector<IoDev*> src; // джерела даних.
 
-    for(int i=0;i<5;++i)
+    for(int i=0;i<3;++i)
     {
         RxModbus *t= new RxModbus;
-        t->setObjectName(QString("cf_%1").arg(i+1));
+        t->setObjectName(QString("kf_%1").arg(i+1));
         t->loadList(":/text/list.txt");
         t->setHostName(QString("10.0.1.%1").arg(i+1));
         t->setPort(502);
@@ -39,10 +38,18 @@ int main(int argc, char *argv[])
         src << t;
     }
 
+    RxModbus *t= new RxModbus;
+    t->setObjectName(QString("common"));
+    t->loadList(":/text/list2.txt");
+    t->setHostName(QString("10.0.1.0"));
+    t->setPort(502);
+    r << t;
+    src << t;
+
 
     Logging l(src,1010); // писалка історіх
     QStringList tables;
-    tables << "Cf_1" << "Cf_2" << "Cf_3" << "Cf_4" << "Cf_5" << "trend" ;
+    tables << "kf_1" << "kf_2" << "kf_3" << "trend" ;
     l.setTables(tables);
 
     IoNetServer s(src); // мережевий інтерфейс
@@ -51,11 +58,13 @@ int main(int argc, char *argv[])
 
     //Alert al(&r);
     //al.loadList(":/text/Linovitsa/filters/text/alert.txt");
-    //QObject::connect(&r,SIGNAL(updateData()),&al,SLOT(checkAlert()));
     //QObject::connect(&al,SIGNAL(newAlert(QString)),&s,SLOT(sendAlert(QString)));
-    for(int i;i<5;++i)
+
+    for(int i;i<4;++i)
     {
         QObject::connect(r[i],SIGNAL(Alert(QString)),&s,SLOT(sendAlert(QString)));
+        //QObject::connect(&r,SIGNAL(updateData()),&al,SLOT(checkAlert()));
+
         //r[i]->start();
     }
     r[0]->start();
